1013b8956e68b236b83ad8f2cfc39c4017f9e30d
elinabeltran <elinabeltran@gmail.com>
2020-10-26 10:43:34 -0300

    Primeros archivos

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+let numeroA=0;
+let operador=0;
+let numeroB=0;
+
+
+function restar(){
+
+}
+
+function sumar(){
+    
+}
+
+function multiplicar(numeroA,numeroB){
+return numeroA*numeroB
+}
+
+function dividir(){
+    
+}
---
+++ practica_ciclos.js
@@ -0,0 +1,32 @@
+/*function noParesDeContarImparesHasta(numero){
+    let impares = 0 ;
+
+    for  (let i = 1; i <=numero ; i++){
+    
+        i%2!= 0?impares=impares+i:"";    
+    }
+ return impares
+}
+
+console.log (noParesDeContarImparesHasta(4))
+*/
+
+/*
+function encontreUn5 (numeros){
+    for ( let i = 0; i <=numeros.length ; i++){
+    i==5?console.log("Se encontró un 5!"):console.log (i);
+    }
+}
+console.log (encontreUn5 ([1,2,3,4,5,6]))
+
+*/
+
+function tablaDeMultiplicar(numero) {
+    let i=0;
+    do { i++
+            console.log (numero+" * "+i+" = "+numero*i);
+              }
+     while(i<10)
+    } 
+        console.log(tablaDeMultiplicar(5))
+ 
